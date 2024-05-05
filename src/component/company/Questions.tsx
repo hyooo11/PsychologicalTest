d9f905c5d97aeb53af16bfd5a01e62549d401b70
hyooo11 <tlsgywls1107@naver.com>
2024-05-06 07:42:55 +0900

    feat: 질문지 작업중

--- src/component/company/Questions.tsx
+++ src/component/company/Questions.tsx
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { questions } from "@/data";
 
+interface BarProps {
+  progress: string;
+}
+
 const ProgressWrap = styled.div`
   display: flex;
 `;
@@ -11,56 +15,96 @@ const ProgressBar = styled.div`
   height: 20px;
   border: 1px solid #ccc;
 `;
+const Bar = styled.div<BarProps>`
+  width: ${(props) => props.progress};
+  height: 100%;
+  background-color: red;
+`;
 
 const Question = () => {
   const [questionList, setQuestionList] = useState(questions);
   const [count, setCount] = useState(0);
-  //enfp, istj
-  console.log(questionList);
+
   const [ei, setEi] = useState(0);
-  console.log(ei);
-  const calScore = (
+  const [ns, setNs] = useState(0);
+  const [ft, setFt] = useState(0);
+  const [pj, setPj] = useState(0);
+
+  //점수계산
+  const mbtiScore = (
     index: number,
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
-    e.currentTarget.value === "EI" && index === 0 ? setEi(ei + 1) : setEi(ei);
+    if (e.currentTarget.value === "EI") {
+      index === 0 ? setEi(ei + 1) : setEi(ei - 1);
+    }
+    if (e.currentTarget.value === "NS") {
+      index === 0 ? setNs(ns + 1) : setNs(ns - 1);
+    }
+    if (e.currentTarget.value === "FT") {
+      index === 0 ? setFt(ft + 1) : setFt(ft - 1);
+    }
+    if (e.currentTarget.value === "PJ") {
+      index === 0 ? setPj(pj + 1) : setPj(pj - 1);
+    }
   };
 
+  //점수결과
+  const mbtiResult = () => {
+    let mbti = "";
+    ei > 0 ? (mbti += "e") : (mbti += "i");
+    ns > 0 ? (mbti += "n") : (mbti += "s");
+    ft > 0 ? (mbti += "f") : (mbti += "t");
+    pj > 0 ? (mbti += "p") : (mbti += "j");
+    console.log(mbti);
+  };
+
+  //버튼 클릭시 이벤트
   const questionHandler = (
     index: number,
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
-    // console.log(index);
     setCount(count + 1);
-    calScore(index, e);
+    mbtiScore(index, e);
+    mbtiResult();
   };
   //진행도 바
+  const progress = `${(100 / questionList.length) * count}%`;
+
+  //이전 질문 다시 선택
+
   return (
-    <div>
-      <div>
-        <div>Q.{questionList[0].question}</div>
-        <ProgressWrap>
-          <div>이전 질문 다시선택 버튼</div>
-          <ProgressBar>진행도</ProgressBar>
-        </ProgressWrap>
+    <>
+      {count < questionList.length ? (
         <div>
-          <button
-            onClick={(e) => questionHandler(0, e)}
-            key="0"
-            value={questionList[0].type}
-          >
-            {questionList[0].answer[0]}
-          </button>
-          <button
-            onClick={(e) => questionHandler(1, e)}
-            key="1"
-            value={questionList[0].type}
-          >
-            {questionList[0].answer[1]}
-          </button>
+          <div>
+            <div>{count + 1}</div>
+            <div>Q.{questionList[count].question}</div>
+            <ProgressWrap>
+              <div>이전 질문</div>
+              <ProgressBar>
+                <Bar progress={progress}></Bar>
+              </ProgressBar>
+            </ProgressWrap>
+            <div>
+              {questionList[count].answer.map((data, index) => {
+                return (
+                  <button
+                    onClick={(e) => questionHandler(0, e)}
+                    key={index}
+                    value={questionList[count].type}
+                  >
+                    {data}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 export default Question;
